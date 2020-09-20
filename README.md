3b1c7bb6df5dbed712113c0bbf63d955353d7b46
John Desrosiers <john.desro@gmail.com>
2020-09-20 07:52:53 -0700

    Initial webpack config

---
+++ README.md
@@ -0,0 +1,31 @@
+# Leaflet Quick Start
+
+[Quick Start example](https://leafletjs.com/examples/quick-start/) found in the [Leaflet Documentation](https://leafletjs.com/).
+
+## Installing Development Tools
+
+This app is built using Webpack 4. Several npm scripts have been configured to make building and development easy.
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build
+```
+
+To run a live development server and open in browser:
+
+```sh
+npm start
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) => {
+  const isProd = env && env.prod;
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    devtool: isProd ? "source-map" : "inline-source-map",
+    stats: "errors-only",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src/index.html"),
+      }),
+      new LodashModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        openAnalyzer: false,
+        logLevel: "error",
+      }),
+    ],
+  };
+};
